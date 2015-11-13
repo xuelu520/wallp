6df8a41d4d33d1b97b19a839771c89ec8bcb526d
xuelu <289415287@qq.com>
2015-11-13 23:20:52 +0800

    CI3.0框架初始化

---
+++ application/config/redis.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 15-11-13
+ * Time: 下午7:32
+ */
+$config['socket_type'] = 'tcp';
+$config['host'] = '127.0.0.1';
+$config['password'] = NULL;
+$config['port'] = 6379;
+$config['timeout'] = 5;
