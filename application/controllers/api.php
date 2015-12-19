872c17dc8aa058943ec325dfb8d2cc3617977060
xuelu520 <289415287@qq.com>
2015-12-19 22:21:55 +0800

    添加跨域请求处理方法

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+		header("Access-Control-Allow-Credentials: true");
+		header('Access-Control-Allow-Origin:http://love.wallp.cn');
+		header('Access-Control-Allow-Methods:POST,GET');
+		header('Access-Control-Max-Age:3628800');
+	}
 
 	public function wg_one() {
 		$wg_id = $this->input->get('wg_id',TRUE);
@@ -11,7 +18,7 @@ class Api extends CI_Controller {
 		}
 		//查询数据
 		$this->load->model('wgroup_model','wg_model');
-		$wg = $this->wg_model->api($wg_id);
+		$wg = $this->wg_model->one($wg_id);
 		if(!$wg) {
 			out(FAIL_STATUS,GET_FAIL_MSG);exit;
 		}
