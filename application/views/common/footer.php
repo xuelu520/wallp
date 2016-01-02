d026c7d827c7688ce167c56d23b563791846e41a
xuelu520 <289415287@qq.com>
2016-01-02 22:44:44 +0800

    添加返回顶部公共方法

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -8,6 +8,9 @@
 ?>
 </div><!--/row-->
 <hr>
+<div class="goToTop" id="backToTop">
+    <a rel="nofollow" href="javascript:void(0)" class="toTop"></a>
+</div>
 <footer>
     <p>&copy; makyu 2015-2016 |Misaka Mikito 炮姐赛高</p>
 </footer>
--- public/js/public.js
+++ public/js/public.js
@@ -1,6 +1,23 @@
 /**
  * Created by m_xuelu on 2015/12/14.
  */
+$(function() {
+    //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+    $(window).scroll(function(){
+        if($(window).scrollTop()>600) {
+            $("#backToTop").show();
+        } else {
+            $("#backToTop").hide();
+        }
+    });
+
+
+    //当点击跳转链接后，回到页面顶部位置
+    $("#backToTop").click(function(){
+        $('body,html').animate({scrollTop:0},200);
+        return false;
+    });
+});
 function nav(id) {
     $('.list-group').find('a').removeClass('active');
     $('#'+id).addClass('active');
