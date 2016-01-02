4440f0bb897262f78143fb7336182e788f95d5f8
xuelu520 <289415287@qq.com>
2016-01-02 22:56:56 +0800

    修改滚动显示长度

--- public/js/public.js
+++ public/js/public.js
@@ -4,7 +4,7 @@
 $(function() {
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(window).scroll(function(){
-        if($(window).scrollTop()>600) {
+        if($(window).scrollTop()>400) {
             $("#backToTop").show();
         } else {
             $("#backToTop").hide();
